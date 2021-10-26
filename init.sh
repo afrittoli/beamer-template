b6a3fd8dfa2ec77b23592e6d09b89797e0c5fa6f
Andrea Frittoli <andrea.frittoli@gmail.com>
2021-10-26 21:14:28 +0100

    Add missing style
    Add missing style file, fix init.sh
    
    Signed-off-by: Andrea Frittoli <andrea.frittoli@gmail.com>

--- init.sh
+++ init.sh
@@ -8,9 +8,9 @@
 TITLE=${1:-"Sample Presentation"}
 FILENAME=${2:-"sample"}
 
+mv template.tex "${FILENAME}.tex"
 sed -i "" 's/__TITLE__/'"$TITLE"'/g' *
 sed -i "" 's/__FILE__/'"$FILENAME"'/g' *
-mv template.tex "${FILENAME}.tex"
 
 # Deploy the pre-commit hook
 cp build.sh .git/hooks/pre-commit
